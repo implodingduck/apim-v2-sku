9a1e3f0933fd0c228067e36f1f32be5600f085ca
implodingduck <zsek320@gmail.com>
2025-07-07 13:15:39 -0500

    initial import

---
+++ README.md
@@ -0,0 +1,3 @@
+# apim-v2-sku
+
+Terraform sample for deploying a Premium v2 APIM
---
+++ terraform/main.tf
@@ -0,0 +1,117 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.35.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "=3.1.0"
+    }
+    azapi = {
+      source = "azure/azapi"
+      version = "=2.3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+
+  subscription_id = var.subscription_id
+}
+
+resource "random_string" "unique" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${local.gh_repo}-${random_string.unique.result}-${local.loc_for_naming}"
+  location = var.location
+  tags = local.tags
+}
+
+resource "azurerm_virtual_network" "default" {
+  name                = "${local.func_name}-vnet-${local.loc_for_naming}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.41.0.0/16"]
+
+  tags = local.tags
+}
+
+resource "azurerm_subnet" "fw" {
+  name                 = "AzureFirewallSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.41.0.0/26"]
+}
+
+resource "azurerm_subnet" "fwmgmt" {
+  name                 = "AzureFirewallManagementSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.41.0.64/26"]
+}
+
+resource "azurerm_subnet" "default" {
+  name                 = "default-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.41.1.0/24"]
+}
+
+resource "azurerm_subnet" "apim" {
+  name                 = "apim-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.41.2.0/24"]
+}
+
+
+# create NSG for the subnet
+resource "azurerm_network_security_group" "nsgapim" {
+  name                = "nsg-${local.func_name}-apim"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowHTTPs"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["443"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowApim"
+    priority                   = 1100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["3443"]
+    source_address_prefix      = "ApiManagement"
+    destination_address_prefix = "*"
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.apim.id
+  network_security_group_id = azurerm_network_security_group.nsgapim.id
+}
+
